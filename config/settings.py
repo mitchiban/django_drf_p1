45f3aabc6e12d39c9310cc9888d7ae732e2de77e
mitchiban <mitch@mitchiban.org>
2022-09-04 10:06:10 +1000

    added product detail view

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'djoser',
 
-  #  'product',
+    'product',
   #  'order'
 ]
 
@@ -128,9 +128,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media/'
