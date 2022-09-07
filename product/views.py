1f8f3495a8ed7763b73df308964d679e8edf42eb
mitchiban <mitch@mitchiban.org>
2022-09-07 21:37:52 +1000

    create new view for category product listing

--- product/views.py
+++ product/views.py
@@ -27,3 +27,15 @@ class ProductDetail(APIView):
         product = self.get_object(category_slug, product_slug)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
+
+class CategoryDetail(APIView):
+    def get_object(self, category_slug):
+        try:
+            return Category.objects.get(slug=category_slug)
+        except Category.DoesNotExist:
+            raise Http404
+    
+    def get(self, request, category_slug, format=None):
+        category = self.get_object(category_slug)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
